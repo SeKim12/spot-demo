eb7a7244fac197241772593aee9ddeaba1b6b450
Seungwoo Kim <seungwoo.simon.kim@gmail.com>
2023-09-24 16:54:49 +0900

    commit from root

--- terraform/gce-spot/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "image" {
-    default = "asia-northeast3-docker.pkg.dev/model-sphere-399315/spot/spot_test"
-}
-
-variable "machine_type" {
-    default = "e2-standard-4"
-}
-
-variable "gcs_path" {}
